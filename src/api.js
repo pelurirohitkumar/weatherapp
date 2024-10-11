67dcb067355ff4492cb337b5b86f4679c02ea72a
Peluri Rohit Kumar <pellurirohitkumar@gmail.com>
2024-10-11 21:44:55 +0530

    second commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+ 
+ 
+ 
+export const geoApiOptions = {
+   method: 'GET',
+   headers: {
+       'x-rapidapi-key': '807b9a9e33mshdaf60ba27bc1534p1e1017jsn8d33b141f705',
+       'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+   }
+};
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "acc5ff95f604edd079351fd4ac2e1636";
